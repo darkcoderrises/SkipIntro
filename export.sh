4aaeb5cc84e3b97eacc350a684bfcb84da5e69a8
Harshil Goel <harshil158@gmail.com>
2019-06-10 10:34:28 +0530

    First commit

---
+++ export.sh
@@ -0,0 +1,4 @@
+export CAFFE_ROOT=/home/anuj.rathore/caffe/
+export PYTHONPATH=/home/anuj.rathore/caffe/distribute/python:$PYTHONPATH
+export PYTHONPATH=/home/anuj.rathore/caffe/python:$PYTHONPATH
+source bin/activate
